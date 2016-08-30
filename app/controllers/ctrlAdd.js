c6855d6280016960d5a285afa74f92c29a5ed144
Tatyana Korzun <tatyana.korzun@codex-soft.com>
2016-08-30 07:00:36 +0300

    add api, functionality, design

--- app/controllers/ctrlAdd.js
+++ app/controllers/ctrlAdd.js
@@ -1,8 +1,9 @@
 module.exports = function($scope) {
     var self = this;
     self.$scope = $scope;
-	self.name = 'city';
-    self.add = function(){
-        self.$scope.$close({name: self.name});
+	self.name = 'Minsk';
+    self.close = function(isAction){
+    	var data = isAction ? {name: self.name} : null;
+        self.$scope.$close(data);
     }
 }
--- app/services/serviceApi.js
+++ app/services/serviceApi.js
@@ -2,18 +2,44 @@ module.exports = function($http) {
     var self = this;
     self.$http = $http;
 
-    self.api = 'http://api.openweathermap.org/data/2.5/weather';
+    self.api = 'http://api.openweathermap.org/data/2.5';
     self.appid = '408e514e8bebc5baf6a1c75f4e979456';
 
-    self.get = function(data){
+    self.getDataByCityName = function(data){
     	var data = {
     		appid: self.appid,
-    		q: data.name
+    		q: data.name,
+            units: 'metric'
     	}
     	return self.$http({
             method: 'GET',
-            url: self.api,
+            url: self.api + '/weather',
             params: data
         });
-    }
+    };
+    self.getDataByCoords = function(data){
+        var data = {
+            appid: self.appid,
+            lon: data.lon,
+            lat: data.lat,
+            units: 'metric'
+        }
+        return self.$http({
+            method: 'GET',
+            url: self.api + '/weather',
+            params: data
+        });
+    };
+    self.getDataBySeveralIds = function(data){
+        var data = {
+            appid: self.appid,
+            id: data.ids,
+            units: 'metric'
+        }
+        return self.$http({
+            method: 'GET',
+            url: self.api + '/group',
+            params: data
+        });
+    };
 }
